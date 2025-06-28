dd45c8e36364160d7cc4b56f958cd02c7ebeabef
Ranga Bandara <rangabandara1729@gmail.com>
2025-06-28 23:43:38 +0530

    UI component separation in the views folder

---
+++ Models/DeviceModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapDrive.Models
+{
+    class DeviceModel
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public bool Initialized { get; set; }
+        public string? Status { get; set; }
+        public bool? IsSyncing { get; set; }
+        public int? SyncProgress { get; set; }
+        public DateTime? InitializedDate { get; set; }
+        public DateTime? LastSyncDate { get; set; }
+        public String[]? BackupItems { get; set; }
+    }
+}
